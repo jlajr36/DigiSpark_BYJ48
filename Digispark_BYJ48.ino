0f1eded4b92f5b776c4441e0ab8f568ad70c3755
John Amos <jlajr36@gmail.com>
2018-10-07 08:21:14 -0400

    Add files via upload

---
+++ Digispark_BYJ48.ino
@@ -0,0 +1,96 @@
+//Sweeps BYJ48 ~90 degress.
+//Uses the first 4 pins of the Digispark
+
+#define IN1 0
+#define IN2 1
+#define IN3 2
+#define IN4 3
+
+void setup() {
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+}
+
+void loop() {
+  for(int f = 0; f < 128; f++) {
+    for(int i = 0; i <= 7;i++) {
+      step(i);
+    }
+  }
+  for(int r = 0; r < 128; r++) {
+    for(int j = 7; j >= 0;j--) {
+      step(j);
+    }
+  }                      
+}
+
+void step(int thecase) {
+  switch(thecase) {
+    case 0:
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+      delay(1);
+      break;
+    case 1:
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, HIGH);
+      delay(1);
+      break;
+    case 2:
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+      delay(1);
+      break;
+    case 3:
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+      delay(1);
+      break;
+    case 4:
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+      delay(1);
+      break;
+    case 5:
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);;
+      delay(1);
+      break;
+    case 6:
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+      delay(1);
+      break;
+    case 7:
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+      delay(1);
+      break;
+    default:
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+      break;
+  }
+}
+
+
